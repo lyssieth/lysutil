c516beb8b89fc9d3720af3d8e6dd776d01b9b180
Rax Ixor <raxixor@rax.ee>
2020-06-10 12:24:36 +0300

    yeah no

--- src/config/json.rs
+++
@@ -1,9 +0,0 @@
-use super::ConfigError;
-
-trait Config {
-    fn path(&self) -> String;
-
-    fn read() -> Result<Box<Self>, ConfigError> {
-        Err(ConfigError::default())
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,4 @@
 //!
 //! I use it in most of my projects, or intend to.
 
-pub mod config;
 pub mod pretty;
