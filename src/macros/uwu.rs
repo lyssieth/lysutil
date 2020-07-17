74c88087684ebc4dcc9d65acff35b1f6fc837740
Lyssieth <raxixor@rax.ee>
2020-07-17 18:35:34 +0300

    updated macro

--- src/macros/uwu.rs
+++ src/macros/uwu.rs
@@ -2,14 +2,14 @@
 /// uwu!
 macro_rules! uwu {
     () => {
-        eprintln!("uwu! [{}:{}]", file!(), line!());
+        eprintln!("uwu![{}:{}]", file!(), line!());
     };
     ($val:expr) => {
         // Use of `match` here is intentional because it affects the lifetimes
         // of temporaries - https://stackoverflow.com/a/48732525/1063961
         match $val {
             tmp => {
-                eprintln!("uwu! [{}:{}] {} = {:#?}",
+                eprintln!("uwu![{}:{}] {} = {:#?}",
                     file!(), line!(), stringify!($val), &tmp);
                 tmp
             }
