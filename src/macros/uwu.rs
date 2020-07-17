72f622a0aab673ac012295da7543dec37eb48e5f
Lyssieth <raxixor@rax.ee>
2020-07-17 18:31:16 +0300

    maybe a macro fix

--- src/macros/uwu.rs
+++ src/macros/uwu.rs
@@ -2,15 +2,15 @@
 /// uwu!
 macro_rules! uwu {
     () => {
-        $crate::eprintln!("uwu! [{}:{}]", $crate::file!(), $crate::line!());
+        eprintln!("uwu! [{}:{}]", file!(), line!());
     };
     ($val:expr) => {
         // Use of `match` here is intentional because it affects the lifetimes
         // of temporaries - https://stackoverflow.com/a/48732525/1063961
         match $val {
             tmp => {
-                $crate::eprintln!("uwu! [{}:{}] {} = {:#?}",
-                    $crate::file!(), $crate::line!(), $crate::stringify!($val), &tmp);
+                eprintln!("uwu! [{}:{}] {} = {:#?}",
+                    file!(), line!(), stringify!($val), &tmp);
                 tmp
             }
         }
